64565041b0937bc492a9c002fa94373c1655053b
Moises Sanchez <mo.sanchez@duocuc.cl>
2024-10-31 14:35:40 -0300

    Agregado de run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-ionic cap run android -l --external --host=192.168.100.230 --port=8100 --target R58T614VCGP
+rem ionic cap run android -l --external --host=192.168.100.37 --port=8100 --target=R58T614VCGP
+ionic cap run android -l --external --host=192.168.1.125 --port=8101 --target=R58M548KGNZ
